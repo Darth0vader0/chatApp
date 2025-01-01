2511578e66069db8cd0cd133dfe554c7e40fb441
Kuru <kamalmajethiya61942@gmail.com>
2025-01-01 23:24:50 +0530

    app.jsx changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 export default function App() {
   return (
     <div>
-      <nav></nav>
+       <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-ghost">Ghost</button>
     </div>
    
   )
