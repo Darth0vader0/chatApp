c97260af5c85997a770c3e712ca1feb47a8cceb8
Kuru <kamalmajethiya@gmail.com>
2025-01-07 23:22:21 +0530

    litte changes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,17 +14,17 @@ const App=() =>{
         }, [checkAuth]); 
 
         console.log({authUser});
-        // if(isCheckingAuth && !authUser)
-        //   return (
-        //       <div className="flex   items-center justify-center h-screen">
-        //             <div className="flex w-52 flex-col gap-4">
-        //                   <div className="skeleton h-32 w-full"></div>
-        //                   <div className="skeleton h-4 w-28"></div>
-        //                   <div className="skeleton h-4 w-full"></div>
-        //                   <div className="skeleton h-4 w-full"></div>
-        //              </div>
-        //       </div>
-        //   );
+        if(isCheckingAuth && !authUser)
+          return (
+              <div className="flex   items-center justify-center h-screen">
+                    <div className="flex w-52 flex-col gap-4">
+                          <div className="skeleton h-32 w-full"></div>
+                          <div className="skeleton h-4 w-28"></div>
+                          <div className="skeleton h-4 w-full"></div>
+                          <div className="skeleton h-4 w-full"></div>
+                     </div>
+              </div>
+          );
         
   return (
     
